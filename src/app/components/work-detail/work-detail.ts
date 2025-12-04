8642efb02a3e60ad4f001921752a7038efa1dff4
Brad Wyatt <bew018@gmail.com>
2025-12-04 07:32:12 -0800

    Add Creative Works Manager functionality

---
+++ src/app/components/work-detail/work-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-detail',
+  imports: [],
+  templateUrl: './work-detail.html',
+  styleUrl: './work-detail.css',
+})
+export class WorkDetail {
+
+}
