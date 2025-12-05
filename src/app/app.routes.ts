ba2b2e68427b41d46c6cf45eac78e6d55d3d4957
Brad Wyatt <bew018@gmail.com>
2025-12-05 08:40:51 -0800

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { WorksListComponent } from './components/works-list/works-list.component';
+import { WorkDetailComponent } from './components/work-detail/work-detail.component';
+import { WorkFormComponent } from './components/work-form/work-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WorksListComponent },
+  { path: 'work/:id', component: WorkDetailComponent },
+  { path: 'add', component: WorkFormComponent },
+  { path: 'edit/:id', component: WorkFormComponent },
+  { path: '**', redirectTo: '' }
+];
