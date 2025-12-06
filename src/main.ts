c79dccd759d303b155d6f7d2dcdec8bf14d14ecf
Brad Wyatt <bew018@gmail.com>
2025-12-06 14:13:38 -0800

    Add archer background and updated theming

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js'; // 👈 this MUST exist
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
